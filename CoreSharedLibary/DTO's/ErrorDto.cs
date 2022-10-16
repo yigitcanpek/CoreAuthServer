f3f577ca360df3cf59fb5d40c3b09c9b8825f1a8
Yiğit Can Pekgüzel <yigitcanpekguzel@gmail.com>
2022-10-17 00:00:27 +0300

    Add project files.

---
+++ CoreSharedLibary/DTO's/ErrorDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreSharedLibary.DTO_s
+{
+    public class ErrorDto
+    {
+        public List<String> Errors { get; private set; }
+
+        public bool IsShow { get; set; }
+
+        public ErrorDto()
+        {
+            Errors = new List<String>();
+        }
+
+        public ErrorDto(string error,bool isShow)
+        {
+            Errors.Add(error);
+            isShow = true;
+        }
+
+        public ErrorDto(List<string> errors,bool isShow)
+        {
+            Errors = Errors;
+            IsShow = isShow;
+        }
+    }
+}
