f1c1b06bada6ce1aff2cc8fc2a5993a4c5f6c637
Yiğit Can Pekgüzel <yigitcanpekguzel@gmail.com>
2022-10-29 19:27:23 +0300

    CoreAuthServer Controller'ları yazıldı.

---
+++ CoreAuthServer.API/Controllers/AuthController.cs
@@ -0,0 +1,48 @@
+﻿using CoreAuthServer.Core.DTO_s;
+using CoreAuthServer.Core.Services;
+using CoreSharedLibary.DTO_s;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreAuthServer.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AuthController : CustomBaseController
+    {
+        private readonly IAuthenticationService _authenticationService;
+
+        public AuthController(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateToken(SignDto sign)
+        {
+            Response<TokenDto> result = await _authenticationService.CreateToken(sign);
+            return ActionResultInstance(result);
+        }
+
+        [HttpPost]
+        public IActionResult CreateTokenByClient(ClientLoginDto clientLoginDto)
+        {
+            Response<ClientTokenDto> result =  _authenticationService.CreateTokenByClient(clientLoginDto);
+            return ActionResultInstance(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RevokeRefreshToken(RefreshTokenDto refreshTokenDto)
+        {
+            Response<TokenDto> result = await _authenticationService.CreateByRefreshToken(refreshTokenDto.Token);
+            return ActionResultInstance(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTokenByRefreshToken(RefreshTokenDto refreshTokenDto)
+        {
+            Response<TokenDto> result = await _authenticationService.CreateByRefreshToken(refreshTokenDto.Token);
+            return ActionResultInstance(result);
+        }
+    }
+}
---
+++ CoreAuthServer.API/Controllers/CustomBaseController.cs
@@ -0,0 +1,16 @@
+﻿using CoreSharedLibary.DTO_s;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreAuthServer.API.Controllers
+{
+    public class CustomBaseController : ControllerBase
+    {
+      public IActionResult ActionResultInstance<T>(Response<T> response)where T : class
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            };
+        }
+    }
+}
